cb2020e7d5b2b061e934c1b953ace87872f193d4
MhmdFayedh <mhmdfayedh@gmail.com>
2023-03-14 22:44:17 +0300

    Add Category & Label Relationships

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -14,4 +14,14 @@ class Ticket extends Model
         'status'
     ];
     use HasFactory;
+    
+    public function category() 
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
+    public function label()
+    {
+        return $this->belongsTo(Label::class);
+    }
 }
